71658eb1298ad8c53c575e22dbe0b8176e272b0c
Akshay KAlola <akshay.kalola28@gmail.com>
2020-06-18 17:51:49 +0530

    design screens

--- app/src/main/java/com/akshaykalola/newsapp/adapters/ArticleAdapter.kt
+++ app/src/main/java/com/akshaykalola/newsapp/adapters/ArticleAdapter.kt
@@ -8,8 +8,20 @@ import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.akshaykalola.newsapp.R
 import com.akshaykalola.newsapp.models.Article
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.item_article.view.*
 
 class ArticleAdapter : RecyclerView.Adapter<ArticleAdapter.ArticleVH>() {
+
+    private val differCallback = object : DiffUtil.ItemCallback<Article>() {
+        override fun areItemsTheSame(oldItem: Article, newItem: Article): Boolean =
+            oldItem.url == newItem.url
+
+        override fun areContentsTheSame(oldItem: Article, newItem: Article): Boolean =
+            oldItem == newItem
+    }
+
+    val articles = AsyncListDiffer(this, differCallback)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ArticleVH {
         return ArticleVH(
@@ -22,20 +34,25 @@ class ArticleAdapter : RecyclerView.Adapter<ArticleAdapter.ArticleVH>() {
     override fun onBindViewHolder(holder: ArticleVH, position: Int) {
         val article = articles.currentList[position]
         holder.itemView.apply {
-
+            Glide.with(this)
+                .load(article.urlToImage).into(ivArticleImage)
+            tvTitle.text = article.title
+            tvSource.text = article.source.name
+            tvDescription.text = article.description
+            tvPublishedAt.text = article.publishedAt
+            setOnClickListener {
+                onItemClickListener?.let {
+                    it(article)
+                }
+            }
         }
     }
 
-    private val differCallback = object : DiffUtil.ItemCallback<Article>() {
-        override fun areItemsTheSame(oldItem: Article, newItem: Article): Boolean =
-            oldItem.url == newItem.url
+    private var onItemClickListener: ((Article) -> Unit)? = null
 
-        override fun areContentsTheSame(oldItem: Article, newItem: Article): Boolean =
-            oldItem == newItem
-
+    fun setOnItemClickListener(listener: (Article) -> Unit) {
+        onItemClickListener = listener
     }
-
-    val articles = AsyncListDiffer(this, differCallback)
 
     inner class ArticleVH(itemView: View) : RecyclerView.ViewHolder(itemView)
 }
